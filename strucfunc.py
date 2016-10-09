5529df1a213bb1e7dbdced1582bda583677e5cf2
William Henney <will@henney.org>
2016-10-09 00:17:54 -0500

    Finish the timing experiments
    Numba version is clear winner.  Closes #1. Closes #2

--- strucfunc.py
+++ strucfunc.py
@@ -52,6 +52,46 @@ def strucfunc_numpy(vmap, dlogr=0.15, wmap=None, wmin_factor=1e-3):
             'Unweighted B(r)': sf/nsf,
             'Weighted B(r)': wsf/weight}
 
+import numba
+
+@numba.jit
+def strucfunc_numba(vmap, dlogr=0.15, wmap=None, wmin_factor=1e-3):
+    """Calculate structure function via naive python algorithm"""
+    ny, nx = vmap.shape
+    if wmap is None:
+        wmap = np.ones_like(vmap)
+    wmin = wmin_factor*np.nanmax(wmap)
+    maxr = np.hypot(nx, ny)
+    nr = int(np.log10(maxr)/dlogr)
+    logr = np.arange(nr)*dlogr
+    sf = np.zeros_like(logr)
+    nsf = np.zeros_like(logr).astype(int)
+    wsf = np.zeros_like(logr)
+    weight = np.zeros_like(logr)
+
+    for j in range(ny):
+        for i in range(nx):
+            for jj in range(ny):
+                for ii in range(i+1, nx):
+                    r = np.hypot(ii - i, jj - j)
+                    ir = int(np.log10(r)/dlogr)
+                    if 0 <= ir < nr:
+                        dvsq = (vmap[jj, ii] - vmap[j, i])**2
+                        if (wmap[j, i] > wmin) and (wmap[jj, ii] > wmin):
+                            sf[ir] += dvsq
+                            nsf[ir] += 1
+                        w = wmap[j, i]*wmap[jj, ii]
+                        wsf[ir] += w*dvsq
+                        weight[ir] += w
+                
+    return {'log10 r': logr,
+            'Sum dv^2': sf,
+            'Sum weights': weight,
+            'Sum w * dv^2': wsf,
+            'N pairs': nsf,
+            'Unweighted B(r)': sf/nsf,
+            'Weighted B(r)': wsf/weight}
+
 def strucfunc_python(vmap, dlogr=0.15, wmap=None, wmin_factor=1e-3):
     """Calculate structure function via naive python algorithm"""
     ny, nx = vmap.shape
@@ -80,7 +120,7 @@ def strucfunc_python(vmap, dlogr=0.15, wmap=None, wmin_factor=1e-3):
                         w = wmap[j, i]*wmap[jj, ii]
                         wsf[ir] += w*dvsq
                         weight[ir] += w
-                  
+                
     return {'log10 r': logr,
             'Sum dv^2': sf,
             'Sum weights': weight,
