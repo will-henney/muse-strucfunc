fdc82dcdcb917fd0c55d09a86394ed2cddbe81ed
William Henney <will@henney.org>
2021-01-26 19:00:24 -0600

    Revive the multi-threaded strucfunc code - super fast now!

--- strucfunc.py
+++ strucfunc.py
@@ -54,9 +54,8 @@ def strucfunc_numpy(vmap, dlogr=0.15, wmap=None, wmin_factor=1e-3):
             'Unweighted B(r)': sf/nsf,
             'Weighted B(r)': wsf/weight}
 
-def strucfunc_numba_futures(vmap, dlogr=0.15,
-                            wmap=None, wmin_factor=1e-3, numthreads=4):
-    """Calculate structure function via jitted, threaded python algorithm"""
+def strucfunc_numba_parallel(vmap, dlogr=0.15, wmap=None, wmin_factor=1e-3):
+    """Calculate structure function via naive python algorithm"""
     ny, nx = vmap.shape
     if wmap is None:
         wmap = np.ones((ny, nx))
@@ -64,69 +63,16 @@ def strucfunc_numba_futures(vmap, dlogr=0.15,
     maxr = np.hypot(nx, ny)
     nr = int(np.log10(maxr)/dlogr)
     logr = np.arange(nr)*dlogr
-
-    # arrays to hold the total accumulation sums
-    sf = np.zeros_like(logr)
-    nsf = np.zeros_like(logr).astype(int)
-    wsf = np.zeros_like(logr)
-    weight = np.zeros_like(logr)
-
-    @numba.jit(nogil=True, nopython=True)
-    def _strucfunc_chunk(j1, j2):
-        """Pure python function designed to be jitted and threaded.
-
-        Finds partial contribution to structure function from a chunk
-        (vmap[j1:j2, :] - vmap[:, :])^2
-        """
-        # Per-thread arrays of partial accumulation sums
-        _sf = np.zeros(nr)
-        _nsf = np.zeros(nr, dtype=np.int64)
-        _wsf = np.zeros(nr)
-        _weight = np.zeros(nr)
-        # outer loop is over only those rows that are in chunk 
-        for j in range(j1, j2):
-            for i in range(nx):
-                for jj in range(ny):
-                    for ii in range(i+1, nx):
-                        r = np.hypot(ii - i, jj - j)
-                        ir = int(np.log10(r)/dlogr)
-                        if 0 <= ir < nr:
-                            dvsq = (vmap[jj, ii] - vmap[j, i])**2
-                            if (wmap[j, i] > wmin) and (wmap[jj, ii] > wmin):
-                                _sf[ir] += dvsq
-                                _nsf[ir] += 1.0
-                            w = 1.0 # wmap[j, i]*wmap[jj, ii]
-                            _wsf[ir] = _wsf[ir] + w*dvsq
-                            _weight[ir] = _weight[ir] + w
-        # return the partial accumulated sums for this chunk
-        return _sf, _nsf, _wsf, _weight
-
-    # Do integer division with remainder
-    chunklen, extras = divmod(ny, numthreads)
-    # So "normal" chunks have length chunklen
-    # While the first `extras` chunks have length (chunklen  + 1)
-    long_chunks = [[i*(chunklen + 1), (i+1)*(chunklen + 1)]
-                   for i in range(extras)]
-    i0 = extras*(chunklen + 1)
-    normal_chunks = [[i*chunklen + i0, (i+1)*chunklen + i0]
-                     for i in range(chunklen - extras)]
-    chunklimits = long_chunks + normal_chunks
-
-    # Spawn one thread per chunk
-    with futures.ThreadPoolExecutor(max_workers=numthreads) as ex:
-        chunks = [
-            ex.submit(_strucfunc_chunk, j1, j2)
-            for j1, j2 in chunklimits
-        ]
-        for f in futures.as_completed(chunks):
-            # As each thread finishes, add its partial result into the
-            # arrays
-            _sf, _nsf, _wsf, _weight = f.result()
-            sf += np.array(_sf)
-            nsf += np.array(_nsf)
-            wsf += np.array(_wsf)
-            weight += np.array(_weight)
-
+    sf = np.zeros((nr,))
+    nsf = np.zeros((nr,), dtype=np.int64)
+    wsf = np.zeros((nr,))
+    weight = np.zeros((nr,))
+    sf, weight, wsf, nsf = _strucfunc_numba_parallel(
+        ny, nx, nr,
+        vmap, wmap,
+        wmin, dlogr, maxr,
+        sf, weight, wsf, nsf
+    )
     return {'log10 r': logr,
             'Sum dv^2': sf,
             'Sum weights': weight,
@@ -135,21 +81,109 @@ def strucfunc_numba_futures(vmap, dlogr=0.15,
             'Unweighted B(r)': sf/nsf,
             'Weighted B(r)': wsf/weight}
 
-@numba.jit
+
+@numba.jit(nopython=True, parallel=True)
+def _strucfunc_numba_prange(
+        ny, nx, nr,
+        vmap, wmap,
+        wmin, dlogr, maxr,
+        sf, weight, wsf, nsf
+):
+    # THIS ONE DOES NOT WORK !!!!!!
+    # Parallel loop on the outer axis
+    for j in numba.prange(ny):
+        # make per-thread containers to hold partial sums
+        _sf = np.zeros(nr)
+        _weight = np.zeros(nr)
+        _wsf = np.zeros(nr)
+        _nsf = np.zeros(nr, dtype=np.int64)
+        for i in range(nx):
+            for jj in range(ny):
+                for ii in range(i+1, nx):
+                    r = np.hypot(ii - i, jj - j)
+                    ir = int(np.log10(r)/dlogr)
+                    if 0 <= ir < nr:
+                        dvsq = (vmap[jj, ii] - vmap[j, i])**2
+                        if (wmap[j, i] > wmin) and (wmap[jj, ii] > wmin):
+                            _sf[ir] += dvsq
+                            _nsf[ir] += 1
+                        w = wmap[j, i]*wmap[jj, ii]
+                        _wsf[ir] += w*dvsq
+                        _weight[ir] += w
+        # Now add the partial sums into the global sums
+        sf += _sf
+        weight += _weight
+        wsf += _wsf
+        nsf += _nsf
+    return sf, weight, wsf, nsf
+
+
+@numba.jit(nopython=True, parallel=True)
+def _strucfunc_numba_parallel(
+        ny, nx, nr,
+        vmap, wmap,
+        wmin, dlogr, maxr,
+        sf, weight, wsf, nsf
+):
+    # Use per-j containers to hold partial results 
+    _sf = np.zeros((ny, nr))
+    _weight = np.zeros((ny, nr))
+    _wsf = np.zeros((ny, nr))
+    _nsf = np.zeros((ny, nr), dtype=np.int64)
+    for j in numba.prange(ny):
+        for i in range(nx):
+            for jj in range(ny):
+                for ii in range(i+1, nx):
+                    r = np.hypot(ii - i, jj - j)
+                    ir = int(np.log10(r)/dlogr)
+                    if 0 <= ir < nr:
+                        dvsq = (vmap[jj, ii] - vmap[j, i])**2
+                        if (wmap[j, i] > wmin) and (wmap[jj, ii] > wmin):
+                            _sf[j, ir] += dvsq
+                            _nsf[j, ir] += 1
+                        w = wmap[j, i]*wmap[jj, ii]
+                        _wsf[j, ir] += w*dvsq
+                        _weight[j, ir] += w
+    sf = np.sum(_sf, axis=0)
+    weight = np.sum(_weight, axis=0)
+    wsf = np.sum(_wsf, axis=0)
+    nsf = np.sum(_nsf, axis=0)
+    return sf, weight, wsf, nsf
+
 def strucfunc_numba(vmap, dlogr=0.15, wmap=None, wmin_factor=1e-3):
     """Calculate structure function via naive python algorithm"""
     ny, nx = vmap.shape
     if wmap is None:
-        wmap = np.ones_like(vmap)
+        wmap = np.ones((ny, nx))
     wmin = wmin_factor*np.nanmax(wmap)
     maxr = np.hypot(nx, ny)
     nr = int(np.log10(maxr)/dlogr)
     logr = np.arange(nr)*dlogr
-    sf = np.zeros_like(logr)
-    nsf = np.zeros_like(logr).astype(int)
-    wsf = np.zeros_like(logr)
-    weight = np.zeros_like(logr)
+    sf = np.zeros((nr,))
+    nsf = np.zeros((nr,), dtype=np.int64)
+    wsf = np.zeros((nr,))
+    weight = np.zeros((nr,))
+    sf, weight, wsf, nsf = _strucfunc_numba(
+        ny, nx, nr,
+        vmap, wmap,
+        wmin, dlogr, maxr,
+        sf, weight, wsf, nsf
+    )
+    return {'log10 r': logr,
+            'Sum dv^2': sf,
+            'Sum weights': weight,
+            'Sum w * dv^2': wsf,
+            'N pairs': nsf,
+            'Unweighted B(r)': sf/nsf,
+            'Weighted B(r)': wsf/weight}
 
+@numba.jit(nopython=True)
+def _strucfunc_numba(
+        ny, nx, nr,
+        vmap, wmap,
+        wmin, dlogr, maxr,
+        sf, weight, wsf, nsf
+):
     for j in range(ny):
         for i in range(nx):
             for jj in range(ny):
@@ -164,14 +198,7 @@ def strucfunc_numba(vmap, dlogr=0.15, wmap=None, wmin_factor=1e-3):
                         w = wmap[j, i]*wmap[jj, ii]
                         wsf[ir] += w*dvsq
                         weight[ir] += w
-
-    return {'log10 r': logr,
-            'Sum dv^2': sf,
-            'Sum weights': weight,
-            'Sum w * dv^2': wsf,
-            'N pairs': nsf,
-            'Unweighted B(r)': sf/nsf,
-            'Weighted B(r)': wsf/weight}
+    return sf, weight, wsf, nsf
 
 def strucfunc_python(vmap, dlogr=0.15, wmap=None, wmin_factor=1e-3):
     """Calculate structure function via naive python algorithm"""
